f82b3e7e529c8f8efae5fb81c1d01e2d85840a57
hastalafiesta- <mauroscianca98@gmail.com>
2014-05-14 20:53:01 +0200

    Added many changes, support from android 2.2

--- gen/com/hasta/balance/R.java
+++ gen/com/hasta/balance/R.java
@@ -8,6 +8,9 @@
 package com.hasta.balance;
 
 public final class R {
+    public static final class array {
+        public static final int tipo_spesa=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -22,26 +25,39 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int custom=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int add=0x7f080001;
-        public static final int balance_value=0x7f080003;
-        public static final int nuovo_bilancio=0x7f080002;
-        public static final int tv=0x7f080000;
+        public static final int action_settings=0x7f090009;
+        public static final int importo=0x7f090001;
+        public static final int incrementa_bilancio=0x7f090004;
+        public static final int nuovo_bilancio=0x7f090003;
+        public static final int sottrai_bilancio=0x7f090005;
+        public static final int sottrai_spesa=0x7f090002;
+        public static final int spesa=0x7f090007;
+        public static final int spinner=0x7f090008;
+        public static final int tv=0x7f090000;
+        public static final int valore_bilancio=0x7f090006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int dialog=0x7f030001;
+        public static final int dialog_nuovo_bilancio=0x7f030001;
+        public static final int dialog_spesa=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int Ok=0x7f050001;
+        public static final int agg_soldi_bilancio=0x7f050007;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int bilancio_iniziale=0x7f050006;
+        public static final int fatto=0x7f050002;
+        public static final int nuova_spesa=0x7f050004;
+        public static final int nuovo_bilancio=0x7f050003;
+        public static final int sottrai_bilancio=0x7f050008;
+        public static final int sottrai_spesa=0x7f050005;
     }
     public static final class style {
         /** 
@@ -65,10 +81,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/hasta/balance/MainActivity.java
+++ src/com/hasta/balance/MainActivity.java
@@ -9,128 +9,230 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
-	private float spesa, s, rimanente, new_balance;
-
+	static float spesa, s, rimanente, nuovo_bilancio, vestiti, cibo, altro;
+	Spinner spn;
+    TextView tv;
+    EditText edit, edit2;
+    Button importo, n_bilancio, incrementa_bilancio, sottrai_spesa, sottrai_bilancio;
+    SharedPreferences prefs;
+    
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Button add=(Button)findViewById(R.id.add);
-		Button nuovo_b=(Button)findViewById(R.id.nuovo_bilancio);
- 	    final TextView tv=(TextView)findViewById(R.id.tv);
-		final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+	    importo=(Button)findViewById(R.id.importo);
+		n_bilancio=(Button)findViewById(R.id.nuovo_bilancio);
+		incrementa_bilancio=(Button)findViewById(R.id.incrementa_bilancio);
+		sottrai_bilancio=(Button)findViewById(R.id.sottrai_bilancio);
+		sottrai_spesa=(Button)findViewById(R.id.sottrai_spesa);
+ 	    tv=(TextView)findViewById(R.id.tv);
+ 	    prefs = PreferenceManager.getDefaultSharedPreferences(this);
 		
-		tv.setText("BILANCIO CORRENTE: "+String.valueOf(prefs.getFloat("balance_value", 0)+" €\n"+"SOLDI SPESI: "+String.valueOf(prefs.getFloat("somma_totale_spese", 0))));
 		
 		if(prefs.getBoolean("first_time", true)) {
 			final LayoutInflater inflater = this.getLayoutInflater();
-		    
-		    final View v=inflater.inflate(R.layout.dialog, null);
+		    final View v=inflater.inflate(R.layout.dialog_nuovo_bilancio, null);
 			 new AlertDialog.Builder 
 					(this)
 		           .setView(v)
-		           .setTitle("Imposta il tuo bilancio iniziale")
-		           .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
-		               @Override
-		               
-		               public void onClick(DialogInterface dialog, int id) { 
-		            	    EditText edit=(EditText)v.findViewById(R.id.balance_value);
-		                   SharedPreferences.Editor editor2 = prefs.edit();
-		                   editor2.putFloat("balance_value", Float.parseFloat(edit.getText().toString()));
-		                   editor2.commit();
-		                   tv.setText("BILANCIO CORRENTE: "+String.valueOf(prefs.getFloat("balance_value", 0)+" €\n"+"SOLDI SPESI: "+String.valueOf(prefs.getFloat("somma_totale_spese", 0))));
-		                   dialog.dismiss();
-		                   SharedPreferences.Editor editor = prefs.edit();
+		           .setTitle(R.string.bilancio_iniziale)
+		           .setNeutralButton(R.string.Ok, new DialogInterface.OnClickListener() {
+		                @Override
+		                public void onClick(DialogInterface dialog, int id) { 
+		            	EditText edit=(EditText)v.findViewById(R.id.valore_bilancio);
+		                SharedPreferences.Editor editor = prefs.edit();
+		                editor.putFloat("bilancio", Float.parseFloat(edit.getText().toString()));
+		                editor.putFloat("rimanente", Float.parseFloat(edit.getText().toString()));
 		       			editor.putBoolean("first_time", false);
 		       			editor.commit();
-		       			
+		       			dialog.dismiss();
+		       			updateTextView();
 		               }
-		               
 		           }).show();
-			
 		}
-		s=prefs.getFloat("somma_totale_spese", 0);
-		add.setOnClickListener(new View.OnClickListener() {
+
+		updateTextView();
+		importo.setOnClickListener(new View.OnClickListener() {
             public void onClick( View v) {
-            	 LayoutInflater inflater = MainActivity.this.getLayoutInflater();
-    		    
-    		    final View v2=inflater.inflate(R.layout.dialog, null);
+            	LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+    		    final View v2=inflater.inflate(R.layout.dialog_spesa, null);
+    		    spn = (Spinner)v2.findViewById(R.id.spinner);
+         	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(MainActivity.this, R.array.tipo_spesa, android.R.layout.simple_spinner_item);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spn.setAdapter(adapter);
             	new AlertDialog.Builder 
 				(MainActivity.this)
 	           .setView(v2)
-	           .setTitle("Inserisci un importo.")
-	           .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+	           .setTitle(R.string.nuova_spesa)
+	           .setNeutralButton(R.string.Ok, new DialogInterface.OnClickListener() {
 	               @Override
-	               
 	               public void onClick(DialogInterface dialog, int id) {
-	            	   
-	            	   EditText edit=(EditText)v2.findViewById(R.id.balance_value);
-
+	            	   edit=(EditText)v2.findViewById(R.id.spesa);
 	                   SharedPreferences.Editor editor = prefs.edit();
 	                   spesa=Float.parseFloat(edit.getText().toString());
-	                   s=s+spesa;
+	                   s=prefs.getFloat("somma_totale_spese", 0)+spesa;
 	                   editor.putFloat("somma_totale_spese", s);
-	                   rimanente=prefs.getFloat("balance_value", 0)-spesa;
-	                   editor.putFloat("balance_value", rimanente);
+	                   rimanente=prefs.getFloat("rimanente", 0)-spesa;
+	                   editor.putFloat("rimanente", rimanente);
+	                   if(spn.getSelectedItemId()==0){
+	                	   cibo=prefs.getFloat("cibo", 0)+spesa;
+	                	   editor.putFloat("cibo", cibo);
+	                   }else if(spn.getSelectedItemId()==1){
+	                	   altro=prefs.getFloat("altro", 0)+spesa;
+	                	   editor.putFloat("altro", altro);
+	                   }else if(spn.getSelectedItemId()==2){
+	                	   vestiti=prefs.getFloat("vestiti", 0)+spesa;
+	                	   editor.putFloat("vestiti", vestiti);
+	                   }
 	                   editor.commit();
-	                   
-	                   tv.setText("BILANCIO CORRENTE: "+String.valueOf(prefs.getFloat("balance_value", 0)+" €\n"+"SOLDI SPESI: "+String.valueOf(prefs.getFloat("somma_totale_spese", 0))));
-	                   
+	                   updateTextView();	                   
 	                   dialog.dismiss();
-	                   if(prefs.getFloat("balance_value", 0)==0){
+	                   if(prefs.getFloat("rimanente", 0)==0){
 	        	     	   Utils.showToast(MainActivity.this, "Sei al verde.");
+	        	        }else if(prefs.getFloat("rimanente",0)<0){
+	        	        	Utils.showToast(MainActivity.this, "Debiti...");
 	        	        }
 	               }
-	               
 	           }).show();
-                
             }
         });
 		
-		nuovo_b.setOnClickListener(new View.OnClickListener() {
+		n_bilancio.setOnClickListener(new View.OnClickListener() {
             public void onClick( View v) {
             	 LayoutInflater inflater = MainActivity.this.getLayoutInflater();
-    		    
-    		    final View v2=inflater.inflate(R.layout.dialog, null);
+    		    final View v2=inflater.inflate(R.layout.dialog_nuovo_bilancio, null);
             	new AlertDialog.Builder 
 				(MainActivity.this)
 	           .setView(v2)
-	           .setTitle("Imposta nuovo bilancio")
-	           .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+	           .setTitle(R.string.nuovo_bilancio)
+	           .setNeutralButton(R.string.fatto, new DialogInterface.OnClickListener() {
 	               @Override
-	               
-	               public void onClick(DialogInterface dialog, int id) {
-	            	   
-	            	   EditText edit=(EditText)v2.findViewById(R.id.balance_value);
-	            	   new_balance=Float.parseFloat(edit.getText().toString());
+	               public void onClick(DialogInterface dialog, int id) {        	   
+	            	   edit2=(EditText)v2.findViewById(R.id.valore_bilancio);
 	            	   s=0;
-	                   SharedPreferences.Editor editor = prefs.edit();
-	                   
-	                   editor.putFloat("balance_value", new_balance);
+	            	   cibo=0;
+	            	   vestiti=0;
+	            	   altro=0;	   
+	            	   nuovo_bilancio=Float.parseFloat(edit.getText().toString());
+	                   SharedPreferences.Editor editor = prefs.edit();                
+	                   editor.putFloat("bilancio", nuovo_bilancio);	                   
 	                   editor.putFloat("somma_totale_spese", 0);
+	                   editor.putFloat("cibo", 0);
+	                   editor.putFloat("vestiti", 0);
+	                   editor.putFloat("altro", 0);	                   	                   
+	                   rimanente=Float.parseFloat(edit.getText().toString());
+	                   editor.putFloat("rimanente", rimanente);
 	                   editor.commit();
-	                   tv.setText("BILANCIO CORRENTE: "+String.valueOf(prefs.getFloat("balance_value", 0)+" €\n"+"SOLDI SPESI: "+String.valueOf(prefs.getFloat("somma_totale_spese", 0))));
-	                   dialog.dismiss();
-	                   if(prefs.getFloat("balance_value", 0)==0){
-	        	     	   Utils.showToast(MainActivity.this, "Sei al verde.");
-	        	        }
+	                   updateTextView();
+	                   dialog.dismiss();     
 	               }
 	               
 	           }).show();
-                
             }
         });
 		
+		sottrai_spesa.setOnClickListener(new View.OnClickListener() {
+            public void onClick( View v) {
+            	 LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+    		    final View v2=inflater.inflate(R.layout.dialog_spesa, null);
+    		    spn = (Spinner)v2.findViewById(R.id.spinner);
+         	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(MainActivity.this, R.array.tipo_spesa, android.R.layout.simple_spinner_item);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spn.setAdapter(adapter);
+            	new AlertDialog.Builder 
+				(MainActivity.this)
+	           .setView(v2)
+	           .setTitle(R.string.sottrai_spesa)
+	           .setNeutralButton(R.string.fatto, new DialogInterface.OnClickListener() {
+	               @Override
+	               public void onClick(DialogInterface dialog, int id) {        	   
+	            	   edit=(EditText)v2.findViewById(R.id.spesa);
+	            	   s=prefs.getFloat("somma_totale_spese", 0)-Float.parseFloat(edit.getText().toString());
+	            	   rimanente=prefs.getFloat("rimanente", 0)+Float.parseFloat(edit.getText().toString());
+	            	   SharedPreferences.Editor editor = prefs.edit();  
+	            	   if(spn.getSelectedItemId()==0){
+	                	   cibo=prefs.getFloat("cibo", 0)-Float.parseFloat(edit.getText().toString());
+	                	   editor.putFloat("cibo", cibo);
+	                   }else if(spn.getSelectedItemId()==1){
+	                	   altro=prefs.getFloat("altro", 0)-Float.parseFloat(edit.getText().toString());
+	                	   editor.putFloat("altro", altro);
+	                   }else if(spn.getSelectedItemId()==2){
+	                	   vestiti=prefs.getFloat("vestiti", 0)-Float.parseFloat(edit.getText().toString());
+	                	   editor.putFloat("vestiti", vestiti);
+	                   }                  
+	                   editor.putFloat("somma_totale_spese", s);	                   
+	                   editor.putFloat("rimanente", rimanente);
+	                   editor.commit();
+	                   updateTextView();
+	                   dialog.dismiss();     
+	               }
+	               
+	           }).show();
+            }
+        });
 		
-			}
+		incrementa_bilancio.setOnClickListener(new View.OnClickListener() {
+            public void onClick( View v) {
+            	 LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+    		    final View v2=inflater.inflate(R.layout.dialog_nuovo_bilancio, null);
+            	new AlertDialog.Builder 
+				(MainActivity.this)
+	           .setView(v2)
+	           .setTitle(R.string.agg_soldi_bilancio)
+	           .setNeutralButton(R.string.fatto, new DialogInterface.OnClickListener() {
+	               @Override
+	               public void onClick(DialogInterface dialog, int id) {        	   
+	            	   edit2=(EditText)v2.findViewById(R.id.valore_bilancio);   
+	            	   nuovo_bilancio=prefs.getFloat("bilancio", 0)+Float.parseFloat(edit2.getText().toString());
+	            	   rimanente=nuovo_bilancio-prefs.getFloat("somma_totale_spese", 0);
+	                   SharedPreferences.Editor editor = prefs.edit();                
+	                   editor.putFloat("bilancio", nuovo_bilancio);	    
+	                   editor.putFloat("rimanente", rimanente);
+	                   editor.commit();
+	                   updateTextView();
+	                   dialog.dismiss();     
+	               }
+	               
+	           }).show();
+            }
+        });
+		
+		sottrai_bilancio.setOnClickListener(new View.OnClickListener() {
+            public void onClick( View v) {
+            	 LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+    		    final View v2=inflater.inflate(R.layout.dialog_nuovo_bilancio, null);
+            	new AlertDialog.Builder 
+				(MainActivity.this)
+	           .setView(v2)
+	           .setTitle(R.string.sottrai_bilancio)
+	           .setNeutralButton(R.string.fatto, new DialogInterface.OnClickListener() {
+	               @Override
+	               public void onClick(DialogInterface dialog, int id) {        	   
+	            	   edit2=(EditText)v2.findViewById(R.id.valore_bilancio);   
+	            	   nuovo_bilancio=prefs.getFloat("bilancio", 0)-Float.parseFloat(edit2.getText().toString());
+	            	   rimanente=nuovo_bilancio-prefs.getFloat("somma_totale_spese", 0);
+	                   SharedPreferences.Editor editor = prefs.edit();
+	                   editor.putFloat("bilancio", nuovo_bilancio);	    
+	                   editor.putFloat("rimanente", rimanente);
+	                   editor.commit();
+	                   updateTextView();
+	                   dialog.dismiss();     
+	               }
+	               
+	           }).show();
+            }
+        });
+	}
 	
-	
-
-
+       public void updateTextView(){   
+    	   tv.setText("BILANCIO: "+String.valueOf(prefs.getFloat("bilancio", 0)+" €\n"+"SOLDI SPESI: "+String.valueOf(prefs.getFloat("somma_totale_spese", 0))+" €\n"+"RIMANENTE: "+String.valueOf(prefs.getFloat("rimanente", 0))+" €\n"+"CIBO: "+String.valueOf(prefs.getFloat("cibo", 0))+" €\n"+"VESTITI: "+String.valueOf(prefs.getFloat("vestiti", 0))+" €\n"+"ALTRO: "+String.valueOf((prefs.getFloat("altro", 0))+" €")));
+       }
 }
